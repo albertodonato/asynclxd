e58d39c1f2a575a5487cb102ecff8e18a93930b6
Alberto Donato <alberto.donato@gmail.com>
2018-03-16 19:12:49 +0100

    drop unused imports

--- aiolxd/api/resources/__init__.py
+++ aiolxd/api/resources/__init__.py
@@ -1,35 +1,16 @@
 """API resources."""
 
-from .certificates import (
-    Certificate,
-    Certificates,
-)
-from .containers import (
-    Container,
-    Containers,
-)
-from .images import (
-    Image,
-    Images,
-)
-from .networks import (
-    Network,
-    Networks,
-)
-from .profiles import (
-    Profile,
-    Profiles,
-)
+from .certificates import Certificates
+from .containers import Containers
+from .images import Images
+from .networks import Networks
+from .profiles import Profiles
+
 
 __all__ = [
-    'Certificate',
     'Certificates',
-    'Container',
     'Containers',
-    'Image',
     'Images',
-    'Network',
     'Networks',
-    'Profile',
     'Profiles',
 ]
